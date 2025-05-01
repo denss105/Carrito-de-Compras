3f37efd139931d8d4b53421300ec1043a71717d4
denisse <denissejoya52000@gmail.com>
2025-05-01 17:14:56 -0600

    Update Usuario.java

--- src/main/java/com/mycompany/carritodecompras/Usuario.java
+++ src/main/java/com/mycompany/carritodecompras/Usuario.java
@@ -8,7 +8,10 @@ import java.util.List;
 
 /**
  *
- * @author Denisse Joya
+ * EQUIPO: Denisse Joya
+           Perla Susana Diaz Rios
+           Evelyn Merari Medina Urdiana 
+           Lesly Danai Osorio Bezares 
  */
 
 public class Usuario {
